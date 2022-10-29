62021088fbffcd97f2c88b6623e46b608497b37a
Viv Vidas <66029009+JustinVidas@users.noreply.github.com>
2022-10-29 17:04:16 -0400

    Update DoorCollider.cs

--- Decaying_Orbit/Assets/Scripts/DoorCollider.cs
+++ Decaying_Orbit/Assets/Scripts/DoorCollider.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 
 public class DoorCollider : MonoBehaviour
 {
-    /*[SerializeField]
-    Collider2D doorColliderTrigger;*/
-    bool key;
+    [SerializeField]
+    bool[] key;
 
     [SerializeField]
     Collider2D doorCollider;
@@ -19,15 +18,19 @@ public class DoorCollider : MonoBehaviour
     void Start()
     {
         doorCollider = gameObject.GetComponent<Collider2D>();
-        key = pockets.Key1;
+        doorCollider.enabled = true;
+        key[0] = pockets.Key1;
     }
 
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        doorCollider.enabled = true;
         Debug.Log(collision.gameObject.name);
-        if (collision.gameObject.name == "Player" && key)
-        { doorCollider.enabled = false; }
+        if (collision.gameObject.name == "Player" && key[0])
+        { 
+            doorCollider.enabled = false; 
+        }
 
     }
 
