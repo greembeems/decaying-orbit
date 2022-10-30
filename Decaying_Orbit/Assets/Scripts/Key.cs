f51595317d6e2e371aeed6ef144905cf6404be04
greembeems <hea4002@g.rit.edu>
2022-10-30 13:25:44 -0400

    added script for keys & updated credits

---
+++ Decaying_Orbit/Assets/Scripts/Key.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    // Player scripts
+    [SerializeField]
+    GameObject player;
+    Pockets playerPocket;
+
+    // Item information
+    [SerializeField]
+    int keyIndex = 0;
+
+    private void Awake()
+    {
+        playerPocket = player.GetComponent<Pockets>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            // Add item to the pocket of the player & disable the item in the scene
+            playerPocket.AddKey(keyIndex);
+            gameObject.SetActive(false);
+        }
+    }
+}
